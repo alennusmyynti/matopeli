552d4ae7d04048297aaa165577047893e6f077bd
eeliss <kodarit.ikonen@gmail.com>
2020-10-22 18:19:19 +0300

    e

--- main.lua
+++ main.lua
@@ -17,8 +17,17 @@ function reset()
 	
 	suunta = 'right'
 	ajastin = 0
+	aikaraja = 0.15
+	ruoki()
 end
 
+function ruoki()
+	ruoka={}
+	ruokaX = love.math.random(1,leveys)
+	ruokaY = love.math.random(1,korkeus)
+	ruoka.x = ruokaX
+	ruoka.y = ruokaY
+end
 --funktio pelin framien piirtämiseen
 function love.draw()
 	
@@ -31,5 +40,93 @@ function love.draw()
 	local function piirraRuutu(x,y)
 		love.graphics.rectangle('fill',(x-1)* ruutu, (y-1) * ruutu, ruutu-1 ,ruutu-1)
 	end
+	
+	love.graphics.setColor(1,0,0)
+	piirraRuutu(ruoka.x,ruoka.y)
+	
+	for indeksi , matopala in ipairs(mato) do
+		love.graphics.setColor(0, 1, 0)
+		piirraRuutu(matopala.x, matopala.y)
+	end
+	
+end
+function love.update(dt)
+
+	ajastin=ajastin+dt
+	
+	if ajastin>=aikaraja then
+	
+	
+		ajastin=ajastin-aikaraja
+		
+	local seuraavaX = mato[1].x
+	local seuraavaY = mato[1].y
+	
+	peliJatkuu=true
+	
+	if suunta == 'right' then
+		seuraavaX = seuraavaX + 1
+		if seuraavaX > leveys then
+		--gameover
+		peliJatkuu=false
+		end
+		
+	elseif suunta =='left' then
+		seuraavaX = seuraavaX - 1
+		if seuraavaX < 1 then
+		--gameover
+		peliJatkuu=false
+		end
+		
+		elseif suunta =='up' then
+		seuraavaY = seuraavaY - 1
+		if seuraavaY > korkeus then
+		--gameover
+		peliJatkuu=false
+		end
+		
+		elseif suunta =='down' then
+		seuraavaY = seuraavaY + 1
+			if seuraavaY < 1 then
+			--gameover
+			peliJatkuu=false
+		end
+		
+	end
+	
+	if peliJatkuu then
+		table.insert(mato,1,{x = seuraavaX, y = seuraavaY})
+		
+		if mato[1].x == ruoka.x and mato[1].y == ruoka.y then
+			ruoki()
+			
+			
+			aikaraja = aikaraja - 0.1
+			
+			if aikaraja <0.1 then
+				aikaraja = 0.1
+			end
+			
+		else
+			table.remove(mato)
+		end
+	end
+
+	
+	end
+end
+
+
+function love.keypressed(key)
+if key == 'right' and peliJatkuu == true and suunta ~= 'left' then
+		suunta = 'right'
+	elseif key == 'left'and peliJatkuu == true and suunta ~= 'right' then
+		suunta = 'left'
+	elseif key == 'up' and peliJatkuu == true and suunta ~= 'down' then
+		suunta = 'up'
+	elseif key == 'down' and peliJatkuu == true and suunta ~= 'up' then
+		suunta = 'down'
+	end
+	
 	
 end
